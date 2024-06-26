18a61238ca1909319aed5a5ed97d9988c9516138
Silver0116ttv <116470725+Silver0116ttv@users.noreply.github.com>
2024-06-25 20:33:42 -0600

    change snake case sript

--- index.sql
+++ index.sql
@@ -40,39 +40,39 @@ CREATE DATABASE name
            [ IS_TEMPLATE [=] istemplate ]
            [ OID [=] oid ]
 
-CREATE TABLE Product (
+CREATE TABLE product (
     P_ID INT PRIMARY KEY,
     Name VARCHAR(255),
     Price DECIMAL(10, 2),
     Description TEXT
 );
 
-CREATE TABLE Order (
+CREATE TABLE order (
     Order_ID INT PRIMARY KEY,
     Order_Amount DECIMAL(10, 2),
     Order_Date DATE
 );
 
-CREATE TABLE Customer (
+CREATE TABLE customer (
     User_ID INT PRIMARY KEY,
     Name VARCHAR(255),
     Email VARCHAR(255),
     Password VARCHAR(255)
 );
 
-CREATE TABLE Payment (
+CREATE TABLE payment (
     Payment_ID INT PRIMARY KEY,
     Type VARCHAR(50),
     Amount DECIMAL(10, 2)
 );
 
-CREATE TABLE Cart (
+CREATE TABLE cart (
     Cart_ID INT PRIMARY KEY,
     User_ID INT,
     FOREIGN KEY (User_ID) REFERENCES Customer(User_ID)
 );
 
-CREATE TABLE Category (
+CREATE TABLE category (
     C_ID INT PRIMARY KEY,
     Name VARCHAR(255),
     Picture VARCHAR(255),
